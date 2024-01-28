386c52491ffc16aad27878aacbb9d84762b388fb
heisen273 <f0xkid@ukr.net>
2024-01-28 21:53:51 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="playlist",
+    version="0.1",
+    packages=["tests"],
+    url="",
+    license="",
+    author="heisen273",
+    author_email="",
+    description="Playlist generation",
+)
